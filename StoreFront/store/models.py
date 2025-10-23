01fbe685afe678ce58d84cdfa91e857d3fae685e
D-Cyberguy <ayotoluwork@gmail.com>
2025-10-23 15:03:38 +0100

    commit with modifications

--- StoreFront/store/models.py
+++ StoreFront/store/models.py
@@ -73,7 +73,7 @@ class Address(models.Model):
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(
         Customer, on_delete=models.CASCADE)
-    zip = models.CharField(max_length=10, null=True)
+    zip = models.CharField(max_length=10)
 
 
 class Cart(models.Model):
