d26996fce34c35ba7413a11e0b0e27c1633b7789
D-Cyberguy <ayotoluwork@gmail.com>
2025-11-06 15:21:49 +0100

    today changes

--- StoreFront/playground/views.py
+++ StoreFront/playground/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from django.db.models import Q, F
+from store.models import Product, OrderItem
 # request -> response
 # request handler
 
+# products ordered & sort by title
+
 
 def say_hello(request):
-    queryset = Product.objects.filter(title__iendswith='set')
+    queryset = Product.objects.filter(
+        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
     return render(request, 'hello.html', {'name': 'Ayomide', 'products': list(queryset)})
