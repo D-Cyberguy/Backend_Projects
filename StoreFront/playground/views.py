d023c53d40130a59ef61f6e2dbf567e778dd64a1
D-Cyberguy <ayotoluwork@gmail.com>
2025-11-11 13:51:47 +0100

    updated changes in tags models and other files

--- StoreFront/playground/views.py
+++ StoreFront/playground/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.db.models import Q, F
-from store.models import Product, OrderItem
+from django.contrib.contenttypes.models import ContentType
+from store.models import Customer, Product
+from tags.models import TaggedItem
 # request -> response
 # request handler
 
 # products ordered & sort by title
 
+# get last 5 orders with their customers and items (incl product)
+
 
 def say_hello(request):
-    queryset = Product.objects.filter(
-        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
-
-    return render(request, 'hello.html', {'name': 'Ayomide', 'products': list(queryset)})
+    queryset = TaggedItem.objects.get_tags_for(Product, 1)
+    return render(request, 'hello.html', {'name': 'Ayomide', 'result': list(queryset)})
